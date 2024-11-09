cdc3dc41fdfb5d973668b8979545177f15b1b0bf
Nikhil Kumar Sharma <nikhilkumarsharman815@gmail.com>
2024-11-09 10:13:46 +0530

    update movie app

--- Movie app/script.js
+++ Movie app/script.js
@@ -12,8 +12,53 @@ document.addEventListener("DOMContentLoaded", () => {
   const genre = document.getElementById("genre");
   const image = document.getElementById("image");
   const info = document.getElementById("info");
+  const listSec = document.getElementById("movieListings");
 
   const API_KEY = "12caba64";
+
+  const movieIDs = [
+    "tt3896198",
+    "tt1190080",
+    "tt13751694",
+    "tt12735488",
+    "tt4574334",
+    "tt12004706",
+    "tt7286456",
+    "tt9389998",
+    "tt0995031",
+    "tt0993846",
+    "tt0993846",
+    "tt0120338",
+  ];
+
+  movieLists(movieIDs);
+
+  async function movieLists(movieIDs) {
+    try {
+      const fetchPromises = movieIDs.map(async (element) => {
+        const urlList = `https://www.omdbapi.com/?i=${element}&apikey=${API_KEY}`;
+        return fetch(urlList).then((response) => response.json());
+      });
+
+      const movies = await Promise.all(fetchPromises);
+
+      movies.forEach((movie) => {
+        const movieCard = document.createElement("div");
+        movieCard.classList.add("movie-card");
+        movieCard.innerHTML = `
+        <div class="media2" style="background-image: url(${movie.Poster}); cursor: pointer;"></div>
+        <div class="card-details">
+        <p style="font-size: 1vw;">${movie.Title}</p>
+        <p class="psize">Runtime: ${movie.Runtime}</p>
+        <p class="psize">Year: ${movie.Year}</p>
+        </div>
+      `;
+        listSec.appendChild(movieCard);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   searchBtn.addEventListener("click", async () => {
     const movie = name.value.trim();
@@ -26,7 +71,6 @@ document.addEventListener("DOMContentLoaded", () => {
       displayMovie(movieDetails);
     } catch (e) {
       console.log(e);
-      ;
     }
   });
 
@@ -60,7 +104,7 @@ document.addEventListener("DOMContentLoaded", () => {
       errorMsg.textContent = `Error: ${Error}`;
     } else {
       info.classList.remove("hidden");
-      errorMsg.classList.add("hidden")
+      errorMsg.classList.add("hidden");
       title.textContent = Title;
       actors.textContent = `Actors: ${Actors}`;
       year.textContent = `Year: ${Year}`;
